479c6a47cc3aa05a38b2359a4d1b38b2409bca6b
Johanz Pineda <118148565+shihansa3@users.noreply.github.com>
2022-12-06 22:30:27 +0800

    Add files via upload

---
+++ exe2 event/script.js
@@ -0,0 +1,22 @@
+function convert_CelToFahr() {
+    // get input user (value) from html
+    let c = document.getElementById("cel_text").value;
+    let f = (c * (9/5)) + 32;
+    // display result inside html
+    document.getElementById("fahr_text").value = `${f} °F`;
+}
+
+
+
+
+
+
+// program to convert celsius to fahrenheit
+// ask the celsius value to the user 
+// const celsius = prompt("Enter a celsius value: ");
+
+// calculate fahrenheit
+// const fahrenheit = (celsius * 1.8) + 32
+
+// display the result
+// console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
